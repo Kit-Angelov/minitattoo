104b55c08ab9fb07f5ac3b7a99d37cdf80a8d4c6
Kit-Angelov <kit.angelov@gmail.com>
2018-12-10 01:07:53 +0300

    restyle

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pic(models.Model):
     image = models.ImageField(upload_to='image')
-    price = models.FloatField(default=1000)
+    price = models.IntegerField(default=1000)
     name = models.CharField(max_length=255)
 
     def __str__(self):
