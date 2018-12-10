198070fb7dd8744ad7a5e0dafc716433288d7cac
Kit-Angelov <kit.angelov@gmail.com>
2018-12-10 23:27:10 +0300

    add image view to admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import mark_safe
 
 
-admin.site.register(Pic)
+@admin.register(Pic)
+class HeroAdmin(admin.ModelAdmin):
+
+    readonly_fields = ["image_view", ]
+
+    def image_view (self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+            )
+    )
